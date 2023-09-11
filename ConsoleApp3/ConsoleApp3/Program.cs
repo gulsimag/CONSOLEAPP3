beff18de99b7175218a3f40d467d125c7281f02f
gulsimag <85019072+gulsimag@users.noreply.github.com>
2023-09-11 21:12:07 +0300

    SAYAC

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi,sayac=0;
+            Console.WriteLine("Bir sayı giriniz: ");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=sayi;i++)
+            {
+                if (sayi%i==0)
+                {
+                    sayac++;
+                }
+            }
+            if (sayac == 2)
+            {
+                Console.WriteLine("Asal Sayı");
+            }
+            else
+            {
+                Console.WriteLine("Asal Sayı değil");
+            }
+        }
+          
+        
+    }
+}
